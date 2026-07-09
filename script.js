9b47562c658fca34583009e50043432fad8c6f4b
Kaitri-cell <karadiyakaitri@gmail.com>
2026-07-09 09:58:00 +0530

    add codes

---
+++ script.js
@@ -0,0 +1,181 @@
+var playerRed = "R";
+var playerYellow = "Y";
+var currPlayer = playerRed;
+
+var gameOver = false;
+var board;
+var currColumns;
+
+var rows = 6;
+var columns = 7;
+let clickSound;
+let winSound;
+let boardElement;
+let winnerElement;
+let playAgainButton;
+
+window.onload = function () {
+    clickSound = document.getElementById("clickSound");
+    winSound = document.getElementById("winSound");
+    boardElement = document.getElementById("board");
+    winnerElement = document.getElementById("winner");
+    playAgainButton = document.getElementById("playAgain");
+    setGame();
+}
+
+function setGame() {
+    board = [];
+    currColumns = [5, 5, 5, 5, 5, 5, 5];
+
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            row.push(' ');
+
+
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile.addEventListener("click", setPiece);
+            boardElement.append(tile);
+        }
+
+        board.push(row);
+    }
+}
+
+function setPiece() {
+    if (gameOver) {
+        return;
+    }
+
+    let coords = this.id.split("-");
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+
+    r = currColumns[c];
+    if (r < 0) {
+        return;
+    }
+
+    board[r][c] = currPlayer;
+
+    clickSound.currentTime = 0;
+    clickSound.play();
+
+    let tile = document.getElementById(r.toString() + "-" + c.toString());
+    if (currPlayer == playerRed) {
+        tile.classList.add("red-piece");
+        currPlayer = playerYellow;
+    }
+    else {
+        tile.classList.add("yellow-piece");
+        currPlayer = playerRed;
+    }
+
+    r -= 1;
+    currColumns[c] = r;
+
+    checkWinner();
+}
+
+function checkWinner() {
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r][c + 1] && board[r][c + 1] == board[r][c + 2] && board[r][c + 2] == board[r][c + 3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+
+    for (let c = 0; c < columns; c++) {
+        for (let r = 0; r < rows - 3; r++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r + 1][c] && board[r + 1][c] == board[r + 2][c] && board[r + 2][c] == board[r + 3][c]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+
+
+    for (let r = 0; r < rows - 3; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r + 1][c + 1] && board[r + 1][c + 1] == board[r + 2][c + 2] && board[r + 2][c + 2] == board[r + 3][c + 3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+
+    for (let r = 3; r < rows; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r - 1][c + 1] && board[r - 1][c + 1] == board[r - 2][c + 2] && board[r - 2][c + 2] == board[r - 3][c + 3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+}
+
+function setWinner(r, c) {
+    let winner = document.getElementById("winner");
+    if (board[r][c] == playerRed) {
+        winner.innerText = "Red Wins";
+    } else {
+        winner.innerText = "Yellow Wins";
+    }
+playAgainButton.style.display = "block";
+    winSound.currentTime = 0;
+    winSound.play();
+    celebrate();
+    gameOver = true;
+}
+
+function celebrate() {
+    const duration = 3000;
+    const end = Date.now() + duration;
+
+    (function frame() {
+        confetti({
+            particleCount: 5,
+            angle: 60,
+            spread: 55,
+            origin: { x: 0 }
+        });
+
+        confetti({
+            particleCount: 5,
+            angle: 120,
+            spread: 55,
+            origin: { x: 1 }
+        });
+
+        if (Date.now() < end) {
+            requestAnimationFrame(frame);
+        }
+    })();
+}
+
+function restartGame() {
+
+    gameOver = false;
+    currPlayer = playerRed;
+
+    winnerElement.innerText = "";
+
+    playAgainButton.style.display = "none";
+
+    boardElement.innerHTML = "";
+
+    setGame();
+
+}
